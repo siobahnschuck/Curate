52f7115e7eb942ce8a5b6472a9f6f9dfabf2ad7c
siobahnschuck <sschuc@saic.edu>
2021-04-18 12:04:48 -0500

    edit profile user info preloaded as placeholder

--- client/src/components/Forms/profileForm.jsx
+++ client/src/components/Forms/profileForm.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import '../../css/Form.css'
 import { connect } from 'react-redux'
-import { addUser, updateProfile } from '../../store/actions/AuthActions'
+import { addUser, getProfileData, updateProfile } from '../../store/actions/AuthActions'
 
 const mapStateToProps = ({ authState }) => {
   return { authState }
@@ -9,12 +10,17 @@ const mapStateToProps = ({ authState }) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     updateAProfile: (id, body) => dispatch(updateProfile(id, body)),
-    addNewUser: (name, value) => dispatch(addUser(name, value))
+    addNewUser: (name, value) => dispatch(addUser(name, value)),
+    fetchProfileData: (id) => dispatch(getProfileData(id)),
   }
 }
 
 const ProfileForm = (props) => {
-  const { registerForm } = props.authState
+  const { registerForm, userInfo, currentUser } = props.authState
+  useEffect(() => {
+    props.fetchProfileData(currentUser.id)
+  }, [])
+
   const handleProfileChange = (e) => {
     props.addNewUser(e.target.name, e.target.value)
   }
@@ -24,38 +30,49 @@ const ProfileForm = (props) => {
     props.updateAProfile(3, registerForm)
   }
   return (
-    <div>
-      <form onSubmit={(e) => handleProfileSubmit(e)}>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={registerForm.username}
-          onChange={(e) => handleProfileChange(e)}
-        />
-        <input
-          type="text"
-          name="email"
-          placeholder="email"
-          value={registerForm.email}
-          onChange={(e) => handleProfileChange(e)}
-        />
-        <input
-          type="text"
-          name="bio"
-          placeholder="bio"
-          value={registerForm.bio}
-          onChange={(e) => handleProfileChange(e)}
-        />
-        <input
-          type="text"
-          name="location"
-          placeholder="location"
-          value={registerForm.location}
-          onChange={(e) => handleProfileChange(e)}
-        />
-        <button onClick={(e) => handleProfileSubmit(e)}>Submit</button>
-      </form>
+    <div className="edit-prof-container">
+      <h1 className="edit-prof-title"> EDIT YOUR PROFILE</h1>
+      <div className="edit-container">
+        <form className="edit-container" onSubmit={(e) => handleProfileSubmit(e)}>
+          <p>Choose a new username</p>
+          <input
+            className="edit-input"
+            type="text"
+            name="username"
+            placeholder={`@${userInfo.username}`}
+            value={registerForm.username}
+            onChange={(e) => handleProfileChange(e)}
+          />
+          <p>Update your email</p>
+          <input
+            className="edit-input"
+            type="email"
+            name="email"
+            placeholder={userInfo.email}
+            value={registerForm.email}
+            onChange={(e) => handleProfileChange(e)}
+          />
+          <p>Public Bio - tell us about yourself</p>
+          <input
+            className="edit-input-bio"
+            type="text"
+            name="bio"
+            placeholder={userInfo.bio}
+            value={registerForm.bio}
+            onChange={(e) => handleProfileChange(e)}
+          />
+          <p>Where in the world are you?</p>
+          <input
+            className="edit-input"
+            type="text"
+            name="location"
+            placeholder={userInfo.location}
+            value={registerForm.location}
+            onChange={(e) => handleProfileChange(e)}
+          />
+          <button className="edit-btn" onClick={(e) => handleProfileSubmit(e)}>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }

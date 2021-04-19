a5f43ab8c37003a7e698e976eabcc8f03416bd6c
siobahnschuck <sschuc@saic.edu>
2021-04-19 12:09:43 -0500

    front deploy var

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 
-const Client = Axios.create({baseURL: 'http://localhost:5000'})
+const Client = Axios.create({
+  baseURL:process.env.NODE_ENV === 'production' ? 
+    process.env.REACT_APP_API_URL : 'http://localhost:5000'})
 
 Client.interceptors.request.use(
   async (config) => {
